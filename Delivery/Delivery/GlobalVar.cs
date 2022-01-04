ac662af5950486554e20cd9676ca44372c67acc7
Lukas Slivka <56702316+luxasscze@users.noreply.github.com>
2022-01-04 16:33:21 +0000

    Update GlobalVar.cs

--- Delivery/Delivery/GlobalVar.cs
+++ Delivery/Delivery/GlobalVar.cs
@@ -7,7 +7,7 @@ namespace Delivery
 {
     public static class GlobalVar
     {
-       public static string _ConnectionString = @"Data Source=SQL5097.site4now.net;Initial Catalog=DB_A63C5C_Joe;User Id=DB_A63C5C_Joe_admin;Password=Kasumi2Goto";
+       public static string _ConnectionString = @"***USE YOUR DB***";
        public static List<Order> _GlobalOrder { get; set; }
 
         public static Location _RestaurantLocation = new Location(52.97997, -1.15285);
